389fa331a3f0f618ed807a49168ca9834537f196
Rene Reede <rene.reede@gmail.com>
2023-08-02 16:17:04 +0300

    Created layout of project

---
+++ 0_Portfolio/Porfolio/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+return (<div className="container">
+    <h1 className="intro">Hi, I'm Rene, a front-end web developer.</h1>
+    <a href="">LinkedIn</a>
+    <a href="">Github</a>
+  </div>
+  )
+}
+
+export default App;
